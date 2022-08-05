7e8d75109ebd40625504b0c9ea4978c8084e209a
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-08-05 14:12:06 -0600

    Add: event listeners

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+//game instantiation
 var game = new Game();
-
+// query selectors
 var gameSection = document.getElementById('table');
 var mainContainer = document.getElementById('mainContainer')
+
+//event listeners
+window.addEventListener('load', displayGame)
+mainContainer.addEventListener('click', getBox)
