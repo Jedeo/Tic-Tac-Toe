1364e2560b0348070219ed72990ecf4677833815
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-08-05 14:18:01 -0600

    Add: displayGame function with innHTL for the DOM

--- main.js
+++ main.js
@@ -7,3 +7,27 @@ var mainContainer = document.getElementById('mainContainer')
 //event listeners
 window.addEventListener('load', displayGame)
 mainContainer.addEventListener('click', getBox)
+
+
+function displayGame() {
+  mainContainer.innerHTML += `
+  <h1 class="player"> It's <span class="toPlayNext"></span>'s Turn </h1>
+  <table id='mainContainer' class="game-bord" id="table">
+    <tr>
+      <td class="border-right box" id="">  </td>
+      <td class="border-bottom box" id="">  </td>
+      <td class="border-left box" id="">  </td>
+    </tr>
+    <tr>
+      <td class="border-right box" id="">  </td>
+      <td class="border-bottom box" id="">  </td>
+      <td class="border-left box" id="">  </td>
+    </tr>
+    <tr>
+      <td class="border-right-only box" id="">  </td>
+      <td class="box" id="">  </td>
+      <td class="border-left-only box" id="">  </td>
+    </tr>
+  </table>`
+
+}
