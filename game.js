0cbfaf446688fb6a8bd39905edd97251403d860b
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-08-06 12:44:24 -0600

    Fix htm css

--- game.js
+++ game.js
@@ -1,12 +1,26 @@
 class Game {
   constructor() {
-    this.playerOne = new Player(1,"❤️")
-    this.playerTwo = new Player(3,"👻")
+    this.playerOne = new Player(1, "❤️")
+    this.playerTwo = new Player(3, "👻")
     this.gameBord = new Board();
-    this.whoseTurn = this.playerOne;
+    this.whoseTurn = this.playerOne.token;
   }
-  updateTurn(currentTurn){
-    this.currentTurn = currentTurn;
+  updateTurn() {
+    var currentPlayer;
+    console.log("in the method");
+    if (this.whoseTurn !== this.playerTwo.token) {
+      console.log("in if statment");
+      currentPlayer = this.playerOne.token
+      this.whoseTurn = this.playerTwo.token;
+      console.log(currentPlayer);
+      return currentPlayer;
+;
+    } else if (this.whoseTurn !== this.playerOne.token) {
+      console.log("IN elseif");
+      currentPlayer = this.playerTwo.token
+      this.whoseTurn = this.playerOne.token;
+      console.log(currentPlayer);
+      return currentPlayer;
+    }
   }
-
 }
--- main.js
+++ main.js
@@ -4,45 +4,42 @@ console.log(game);
 
 //globel variables
 var currentLocation;
-
+var toPlayNext = currentLocation;
 // query selectors
 var gameSection = document.getElementById('table');
-var mainContainer = document.getElementById('mainContainer')
+var tableContainer = document.getElementById('tableContainer')
 
 //event listeners
 window.addEventListener('load', displayGame)
-mainContainer.addEventListener('click', getBox)
+tableContainer.addEventListener('click', getBox)
 
 
 function displayGame() {
-  mainContainer.innerHTML += `
-  <h1 class="player"> It's <span class="toPlayNext"></span>'s Turn </h1>
-  <table id='mainContainer' class="game-bord" id="table">
-    <tr>
-      <td class="border-right box" id=""> 1 </td>
-      <td class="border-bottom box" id=""> 2 </td>
-      <td class="border-left box" id=""> 3 </td>
-    </tr>
-    <tr>
-      <td class="border-right box" id=""> 4 </td>
-      <td class="border-bottom box" id=""> 5 </td>
-      <td class="border-left box" id=""> 6 </td>
-    </tr>
-    <tr>
-      <td class="border-right-only box" id=""> 7 </td>
-      <td class="box" id=""> 8 </td>
-      <td class="border-left-only box" id=""> 9 </td>
-    </tr>
-  </table>`
+  tableContainer.innerHTML += `
+  <tr>
+    <td class="border-right box" id=""> 1 </td>
+    <td class="border-bottom box" id=""> 2 </td>
+    <td class="border-left box" id=""> 3 </td>
+  </tr>
+  <tr>
+    <td class="border-right box" id=""> 4 </td>
+    <td class="border-bottom box" id=""> 5 </td>
+    <td class="border-left box" id=""> 6 </td>
+  </tr>
+  <tr>
+    <td class="border-right-only box" id=""> 7 </td>
+    <td class="box" id=""> 8 </td>
+    <td class="border-left-only box" id=""> 9 </td>
+  </tr>
+  `
 
 }
 
 
 function getBox(){
+  console.log('got clicked');
   if(event.target.classList.contains("box")){
-    var currentPlayer = document.querySelector(".toPlayNext");
-     currentLocation = event.target.closest(".box")
-     currentLocation.innerHTML = "❤️" //
-
+     tableContainer = event.target.closest(".box")
+     tableContainer.innerHTML = game.updateTurn();
   }
 }
